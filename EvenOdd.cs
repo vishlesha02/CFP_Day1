a6cb680ef22f4326f29e3457531a2082fd8b18ee
vishlesha02 <vishleshamortale18@gmail.com>
2023-06-09 12:55:43 +0530

    EvenOdd program added

---
+++ EvenOdd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDemo
+{
+    internal class EvenOdd
+    {
+        public void evodd()
+        {
+            Console.Write("Enter the Number : ");
+            int i = int.Parse(Console.ReadLine());
+
+            if ( i % 2 == 0)
+            {
+                Console.WriteLine($"{i} is an Even Number");
+            }
+            else {
+
+                Console.WriteLine($"{i} is an ODD Number");
+            }
+        }
+    }
+}
