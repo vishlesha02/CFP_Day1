874bea919ad5b6f24ee3400b3acad7d9d4e3ce74
vishlesha02 <vishleshamortale18@gmail.com>
2023-06-09 12:19:52 +0530

    leap year program added

---
+++ LeapYear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDemo
+{
+    class LeapYear
+    {
+        public void leapy()
+        {
+            Console.WriteLine("Enter the Leap Year");
+            int year = int.Parse(Console.ReadLine());
+
+            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+            { 
+                Console.WriteLine($"{year} is a Leap Year ");
+            }
+
+            else
+            {
+                Console.WriteLine($"{year} is not a Leap Year");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using ConsoleAppDemo;
+using System;
+
+namespace Hello
+{
+    class Randomm
+    {
+
+        static void Main(String[] args)
+        {
+            RandomNumber obj = new RandomNumber();
+            obj.coinf();
+
+            /* LeapYear obj = new LeapYear();
+             obj.leapy(); */
+
+            /* HarmonicNumber obj = new HarmonicNumber();
+              obj.harmonic();*/
+
+            /* Factors obj = new Factors();
+             obj.primeFactorization();*/
+
+            /* ComputeQnR obj = new ComputeQnR();
+             obj.compute();*/
+
+
+            /*   SwapNumbers obj = new SwapNumbers();
+               obj.swap();*/
+
+            /* EvenOdd obj = new EvenOdd();
+             obj.evodd();*/
+
+
+            /*vowelCase obj = new vowelCase();
+            obj.vc();*/
+            /*
+                        largestNumber obj = new largestNumber();
+                        obj.largenum();*/
+        }
+
+       
+    }
+}
