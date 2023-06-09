531d212f18992a77c7953c1246c0c690045ae2ab
vishlesha02 <vishleshamortale18@gmail.com>
2023-06-09 12:14:25 +0530

    first commit

---
+++ RandomNumber.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDemo
+{
+    class RandomNumber
+    { 
+        public void coinf()
+        {
+            Console.WriteLine("Enter the Number of times to flip the coin");
+            int numberofFlips = int.Parse(Console.ReadLine());
+
+            int headCount = 0;
+            int tailCount = 0;
+
+            Random random = new Random();
+
+
+            for (int i = 0; i < numberofFlips; i++)
+            {
+
+                /* Random random = new Random();
+                 int randomValue = random.Next(2);*/
+
+                double randomValue = random.NextDouble();
+               
+                if (randomValue < 0.5)
+                {
+                    Console.WriteLine("Tails");
+
+                    tailCount = tailCount + 1;
+                }
+                else
+                {
+                    Console.WriteLine("Head");
+
+                    headCount = headCount + 1;
+                }
+
+            }
+            double t = (double)tailCount / numberofFlips * 100;
+            double h = (double)headCount / numberofFlips * 100;
+
+            Console.WriteLine($"Tails {t} " + double.Parse("0.00") + "%");
+            Console.ReadLine();
+
+            Console.WriteLine($"Heads {h} " +  double.Parse("0.00") + "%");
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
+
