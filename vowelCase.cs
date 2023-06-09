b23910b8a1fbab1f9252ec3d0c89eea483b1961e
vishlesha02 <vishleshamortale18@gmail.com>
2023-06-09 12:59:36 +0530

    vowelConsonant program added

---
+++ vowelCase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDemo
+{
+    internal class vowelCase
+    {
+        public void vc()
+        {
+            Console.WriteLine("Enter the Alphabet");
+            char letter = char.Parse(Console.ReadLine());
+            
+
+            switch (letter)
+            {
+                case 'a':
+                   
+                case 'e':
+                
+                case 'i':
+                   
+                case 'o':
+                 
+                case 'u':
+                    Console.WriteLine($"{letter} is a Vowel");
+                    break;
+
+                default:
+                    Console.WriteLine($"{letter} is a Consonant");
+                    break;
+
+
+            }
+           
+
+        }
+       
+    }
+}
