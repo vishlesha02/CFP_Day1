bdbb321d9ac570cc2e4d9aff49e5d19931da13e2
vishlesha02 <vishleshamortale18@gmail.com>
2023-06-09 12:35:24 +0530

    compute Number program added

---
+++ ComputeQnR.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDemo
+{
+    internal class ComputeQnR
+    {
+        public void compute()
+        {
+            Console.WriteLine("Enter the Divident");
+            int divident = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Divisor");
+            int divisor = int.Parse(Console.ReadLine());
+
+            int q = divident / divisor;
+            int r = divident % divisor;
+
+            Console.WriteLine($"The Quotient is {q}");
+            Console.WriteLine($"The Reminder is {r}");
+
+        }
+    }
+}
